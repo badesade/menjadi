87335fbf488c3a62e135112dd224b04dada80356
bade sa <165251957+badesade@users.noreply.github.com>
2024-03-28 13:49:18 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-57d472f0-0e8d-4f06-9ada-f344482f4d67/)
 
 
 echo "$curl_output" >> Update.tsx
